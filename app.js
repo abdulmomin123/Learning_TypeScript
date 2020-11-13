eeaff40427b1352fe897a85911b3b2eaad447114
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-13 22:52:16 +0600

    all

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+var Roll;
+(function (Roll) {
+    Roll[Roll["ADMIN"] = 0] = "ADMIN";
+    Roll[Roll["READER"] = 1] = "READER";
+    Roll[Roll["AUTHOR"] = 2] = "AUTHOR";
+})(Roll || (Roll = {}));
 // type infersion
 var person = {
     name: 'momin',
     age: 18,
     hobbies: ['none', 'none!'],
-    role: [2, 'dev']
+    role: [Roll.ADMIN, Roll[0]]
 };
 // person.role.push(22);
 // person.role[0] = 22;
-console.log(person.role);
+console.log(Roll);
--- app.ts
+++ app.ts
@@ -1,17 +1,18 @@
+enum Roll {
+  ADMIN,
+  READER,
+  AUTHOR,
+}
+
 // type infersion
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+const person = {
   name: 'momin',
   age: 18,
   hobbies: ['none', 'none!'],
-  role: [2, 'dev'],
+  role: [Roll.ADMIN, Roll[0]],
 };
 
 // person.role.push(22);
 // person.role[0] = 22;
 
-console.log(person.role);
+console.log(Roll);
