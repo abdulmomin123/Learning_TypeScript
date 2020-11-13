bdb48035507334061919a8e51e3ac5b2ee4565e5
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-13 18:44:33 +0600

    all

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-function add(num1, num2, showRes) {
-    if (showRes) {
-        console.log(num1 + num2);
-    }
-    else {
-        return num1 + num2;
-    }
-}
-var n1 = 5, n2 = 2.8, printResult = true;
-var result = add(n1, n2, printResult);
-console.log(result);
+var person = {
+    name: 'momin',
+    age: 18
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,15 +1,6 @@
-function add(num1: number, num2: number, showRes: boolean) {
-  if (showRes) {
-    console.log(num1 + num2);
-  } else {
-    return num1 + num2;
-  }
-}
+const person = {
+  name: 'momin',
+  age: 18,
+};
 
-const n1 = 5,
-  n2 = 2.8,
-  printResult = true;
-
-const result = add(n1, n2, printResult);
-
-console.log(result);
+console.log(person.age);
