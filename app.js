9d2211ca73504e297ccd12dd388e383a9c98f786
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-13 18:13:08 +0600

    all

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('hi');
+function add(num1, num2) {
+    return num1 + num2;
+}
+var n1 = '5', n2 = 2.8;
+var result = add(n1, n2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('hi');
+function add(num1: number, num2: number, showRes: boolean) {
+  return num1 + num2;
+
+  if (showRes) {
+    console.log(result);
+  }
+}
+
+const n1 = 5,
+  n2 = 2.8,
+  printResult = true;
+
+const result = add(n1, n2, printResult);
