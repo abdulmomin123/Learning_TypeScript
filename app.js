74823d0aac2b282e2c95e39d23a9b5a8749b2054
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-13 22:35:16 +0600

    all

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 var person = {
     name: 'momin',
     age: 18,
-    hobbies: ['none', 'none!']
+    hobbies: ['none', 'none!'],
+    role: [2, 'dev']
 };
-person.hobbies.push('22');
-console.log(person.hobbies[0]);
+// person.role.push(22);
+// person.role[0] = 22;
+console.log(person.role);
--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
 // type infersion
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'momin',
   age: 18,
   hobbies: ['none', 'none!'],
+  role: [2, 'dev'],
 };
 
-person.hobbies.push('22');
+// person.role.push(22);
+// person.role[0] = 22;
 
-console.log(person.hobbies[0]);
+console.log(person.role);
