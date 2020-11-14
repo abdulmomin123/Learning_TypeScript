53723af8cbe3e51410a8ecc2e424d855a18541d1
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-14 06:50:23 +0600

    all

---
+++ app.js
@@ -0,0 +1,9 @@
+function add(n1, n2, outputAs) {
+    var result;
+    if (outputAs === 'asNumber') {
+        result = n1 + n2;
+    }
+    else {
+        //
+    }
+}
---
+++ app.ts
@@ -0,0 +1,9 @@
+function add(n1: number, n2: number, outputAs: string) {
+  let result;
+
+  if (outputAs === 'asNumber') {
+    result = n1 + n2;
+  } else {
+    //
+  }
+}
