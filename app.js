bc3425e66f56e0c8899af95623d1ccdce4740342
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-14 15:24:18 +0600

    all

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-var union = [22, 'hi'];
+function add(w1, w2, outputAs) {
+    if (outputAs === 'return') {
+        return w1 + w2;
+    }
+    else {
+        console.log(w1 + w2);
+    }
+}
+add('type', 'script', 'save');
--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-const union: (number | string)[] = [22, 'hi'];
+type outputOptions = 'return' | 'save';
+
+function add(w1: string, w2: string, outputAs: outputOptions) {
+  if (outputAs === 'return') {
+    return w1 + w2;
+  } else {
+    console.log(w1 + w2);
+  }
+}
+
+add('type', 'script', 'save');
