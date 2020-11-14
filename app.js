6e5cb8c5714b724920c4ca2fe4105be38eff3c62
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-14 06:52:47 +0600

    all

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 function add(n1, n2, outputAs) {
     var result;
     if (outputAs === 'asNumber') {
-        result = n1 + n2;
+        result = +n1 + +n2;
     }
     else {
-        //
+        result = n1 + n2;
     }
+    return result;
 }
+var r1 = add(2, 2, 'asNumber');
+var r2 = add(2, 2, 'asString');
+console.log(r1, r2);
--- app.ts
+++ app.ts
@@ -2,8 +2,15 @@ function add(n1: number, n2: number, outputAs: string) {
   let result;
 
   if (outputAs === 'asNumber') {
-    result = n1 + n2;
+    result = +n1 + +n2;
   } else {
-    //
+    result = n1 + n2;
   }
+
+  return result;
 }
+
+const r1 = add(2, 2, 'asNumber');
+const r2 = add(2, 2, 'asString');
+
+console.log(r1, r2);
