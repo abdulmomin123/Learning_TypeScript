b48a1672ca9d6e0b6ffa4970deec6f3a466c14f8
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-14 15:15:38 +0600

    all

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-var mixedArr = ['22', 'hi', 'hi'];
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 10] = "ADMIN";
-    Role[Role["READER"] = 0] = "READER";
-    Role[Role["AUTHOR"] = 1] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: 'momin',
-    age: 18,
-    role: [Role.ADMIN]
-};
-console.log(Role);
+var union = [22, 'hi'];
--- app.ts
+++ app.ts
@@ -1,15 +1,1 @@
-const mixedArr: (number | string)[] = ['22', 'hi', 'hi'];
-
-enum Role {
-  ADMIN = 10,
-  READER = 0,
-  AUTHOR,
-}
-
-const person = {
-  name: 'momin',
-  age: 18,
-  role: [Role.ADMIN],
-};
-
-console.log(Role);
+const union: (number | string)[] = [22, 'hi'];
