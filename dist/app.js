2e1cf6ab550101857edc8a48de143022f8a6842d
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-14 22:00:03 +0600

    all

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,9 @@
 "use strict";
+class Book {
+    constructor(type, name, genre) {
+        this.type = type;
+        this.name = name;
+        this.genre = genre;
+    }
+}
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+class Book {
+  type: string;
+  name: string;
+  genre: string;
+
+  constructor(type: string, name: string, genre: string) {
+    this.type = type;
+    this.name = name;
+    this.genre = genre;
+  }
+}
