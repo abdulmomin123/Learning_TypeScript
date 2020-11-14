0c69287ed9566f0945b36b4713e5700f42904501
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-14 07:14:21 +0600

    all

--- app.js
+++ app.js
@@ -2,5 +2,10 @@
 function add(n1, n2) {
     return n1 + n2;
 }
+// void type
+function greet(name) {
+    return name;
+}
+greet('momin');
 var r1 = add(2, 2);
 console.log(r1);
--- app.ts
+++ app.ts
@@ -1,7 +1,14 @@
 // Function return types & void
-function add(n1: number, n2: number): number {
+function add(n1: number, n2: number) {
   return n1 + n2;
 }
+
+// void type
+function greet(name: string) {
+  return name;
+}
+
+greet('momin');
 
 const r1 = add(2, 2);
 
