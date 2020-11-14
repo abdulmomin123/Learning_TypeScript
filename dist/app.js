b68b4735cad53e389a0dd28dce3923649b844765
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-14 19:28:45 +0600

    all

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,1 @@
 "use strict";
-const button = document.querySelector('button');
-let namee = 'momin';
-button.addEventListener('click', e => {
-    console.log(namee);
-});
--- src/app.ts
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('button')!;
-
-let namee: string = 'momin';
-
-button.addEventListener('click', e => {
-  console.log(namee);
-});
