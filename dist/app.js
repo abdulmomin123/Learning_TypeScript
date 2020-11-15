a9c308035d1cdea46d991d453f3241c0fc917ee9
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-15 08:17:14 +0600

    all

--- dist/app.js
+++ dist/app.js
@@ -1,22 +1,2 @@
 "use strict";
-class ClassRoom {
-    constructor(students, teacher) {
-        this.students = students;
-        this.teacher = teacher;
-    }
-    getStudents() {
-        console.log(this.students);
-    }
-    getTeacher() {
-        console.log(this.teacher);
-    }
-}
-const classTwelve = new ClassRoom([
-    {
-        school: 'Al-Hera',
-        currentClass: 12,
-        roll: 53,
-        ranking: 'low',
-    },
-], { name: 'Usman Gani', education: 'Honors', age: 50 });
 //# sourceMappingURL=app.js.map
