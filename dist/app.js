428c1af61611b26d769d2e81ddb144930cd34f8a
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-15 19:47:12 +0600

    all

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,9 @@
 function add(n1, n2) {
     if (typeof n1 === 'number' && typeof n2 === 'number')
         return n1 + n2;
-    return n1.toString() + ' ' + n2.toString();
+    return n1.toString() + n2.toString();
 }
+const num = add(2, 2);
+const str = add(2, '2');
+console.log(num, str);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,10 @@ type Combinable = string | number;
 function add(n1: Combinable, n2: Combinable) {
   if (typeof n1 === 'number' && typeof n2 === 'number') return n1 + n2;
 
-  return n1.toString() + ' ' + n2.toString();
+  return n1.toString() + n2.toString();
 }
+
+const num = add(2, 2);
+const str = add(2, '2');
+
+console.log(num, str);
