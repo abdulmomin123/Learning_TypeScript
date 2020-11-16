b52ca11c11bbe1ebde41ac209cc2b192e2213b4e
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-16 20:05:45 +0600

    all

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,9 @@
 "use strict";
-class DataStorage {
-    constructor() {
-        this.data = [];
-    }
-    addItem(item) {
-        this.data.push(item);
-    }
-    removeItem(item) {
-        if (this.data.indexOf(item) !== -1)
-            this.data.splice(this.data.indexOf(item), 1);
-    }
-    getItems() {
-        return [...this.data];
-    }
+function planVideo(title, description, publishDate) {
+    let videoBlueprint = {};
+    videoBlueprint.title = title;
+    videoBlueprint.description = description;
+    videoBlueprint.publishDate = publishDate;
+    return videoBlueprint;
 }
-const textStorage = new DataStorage();
-textStorage.addItem('momin');
-textStorage.addItem('mim');
-textStorage.addItem('mim');
-textStorage.removeItem('mim');
-const numberStorage = new DataStorage();
-numberStorage.addItem(22);
-numberStorage.addItem(23);
-numberStorage.removeItem(23);
-console.log(textStorage.getItems());
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,19 @@
-class DataStorage<T extends number | string | boolean> {
-  private data: T[] = [];
-
-  addItem(item: T) {
-    this.data.push(item);
-  }
-
-  removeItem(item: T) {
-    if (this.data.indexOf(item) !== -1)
-      this.data.splice(this.data.indexOf(item), 1);
-  }
-
-  getItems() {
-    return [...this.data];
-  }
+interface Video {
+  title: string;
+  description: string;
+  publishDate: Date;
 }
 
-const textStorage = new DataStorage<string>();
+function planVideo(
+  title: string,
+  description: string,
+  publishDate: Date
+): Video {
+  let videoBlueprint: Partial<Video> = {};
 
-textStorage.addItem('momin');
-textStorage.addItem('mim');
-textStorage.addItem('mim');
-textStorage.removeItem('mim');
+  videoBlueprint.title = title;
+  videoBlueprint.description = description;
+  videoBlueprint.publishDate = publishDate;
 
-const numberStorage = new DataStorage<number>();
-
-numberStorage.addItem(22);
-numberStorage.addItem(23);
-numberStorage.removeItem(23);
-
-// const objectStorage = new DataStorage<object>();
-
-// objectStorage.addItem({ name: 'momin' });
-// objectStorage.addItem({ age: 18 });
-// objectStorage.removeItem({ name: 'momin' });
-
-console.log(textStorage.getItems());
+  return videoBlueprint as Video;
+}
