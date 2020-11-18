b31147241e6015e1618179517d4aae742b4a54c2
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-18 07:51:58 +0600

    all

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,22 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+const btn = document.querySelector('button');
+function autoBind(_target, _method, descriptor) {
+    const initialDes = descriptor.value;
+    const newDes = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            return initialDes.bind(this);
+        },
+    };
+    return newDes;
+}
 class Printer {
     constructor() {
         this.message = 'this works';
@@ -7,4 +25,10 @@ class Printer {
         console.log(this.message);
     }
 }
+__decorate([
+    autoBind
+], Printer.prototype, "showMessage", null);
+const test = new Printer();
+btn.addEventListener('click', test.showMessage);
+test.showMessage();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
+const btn = document.querySelector('button')!;
+
+function autoBind(
+  this: any,
+  _target: any,
+  _method: string,
+  descriptor: PropertyDescriptor
+) {
+  const initialDes = descriptor.value;
+  const newDes: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      return initialDes.bind(this);
+    },
+  };
+
+  return newDes;
+}
+
 class Printer {
   message = 'this works';
 
@@ -5,7 +25,14 @@ class Printer {
     //
   }
 
+  @autoBind
   showMessage() {
     console.log(this.message);
   }
 }
+
+const test = new Printer();
+
+btn.addEventListener('click', test.showMessage);
+
+test.showMessage();
