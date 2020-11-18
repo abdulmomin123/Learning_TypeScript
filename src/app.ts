462ab46c1b75fc84ff65038a4b9e9ec7396ff0d5
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-18 07:18:52 +0600

    all

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,5 @@
-function renderPerson(tag: string, selector: string) {
-  return function <T extends { new (...args: any[]): { name: string } }>(
-    initialConstructor: T
-  ) {
-    return class extends initialConstructor {
-      constructor(...args: any[]) {
-        super();
-        const target = document.querySelector(selector)!;
-
-        console.log(args);
-        if (target) {
-          target.innerHTML = `<${tag}> ${this.name} </${tag}>`;
-        }
-      }
-    };
-  };
+class Printer {
+  constructor() {
+    //
+  }
 }
-
-@renderPerson('h1', 'div')
-class Person {
-  name: string = 'momin';
-
-  constructor() {}
-}
-
-const me = new Person();
-
-console.log(me);
